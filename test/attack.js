f3b055f3b698b754b2bcf131d2fd90c651575b81
Utilitycoder <66361605+Utilitycoder@users.noreply.github.com>
2022-06-01 12:54:21 +0100

    final commit

--- test/attack.js
+++ test/attack.js
@@ -4,24 +4,26 @@ const { expect } = require("chai")
 const usernameBytes = ethers.utils.formatBytes32String("test");
 const passwordBytes = ethers.utils.formatBytes32String("password");
 
-decribe("Attack", async () => {
+describe("Attack", async () => {
     it("It should show that anyone can access private data on blockchain", async () => {
         // Deploy the login contract
-        const Login = ethers.getContractFactory("Login")
+        const Login = await ethers.getContractFactory("Login")
         const login = await Login.deploy(usernameBytes, passwordBytes)
         await login.deployed()
-
+        
+        //Let's request for storage details of contract. 
         const slot0Bytes = await ethers.provider.getStorageAt(
             login.address, 0
         )
 
         const slot1Bytes = await ethers.provider.getStorageAt(login.address, 1)
 
+        // let's log out the parsed bytes received from the slots.
         console.log(ethers.utils.parseBytes32String(slot0Bytes))
         console.log(ethers.utils.parseBytes32String(slot1Bytes))
 
-        // Confirrm the the data received from the slots are the same
-        expect(ethers.utils.parseBytes32String(slot0Bytes)).toLocaleLowerCase.equal("test")
-        expect(ethers.utils.parseBytes32String(slot1Bytes)).toLocaleLowerCase.equal("password")
+        // Confirm the the data received from the slots are the same
+        expect(ethers.utils.parseBytes32String(slot0Bytes)).to.equal("test")
+        expect(ethers.utils.parseBytes32String(slot1Bytes)).to.equal("password")
     })
 })
